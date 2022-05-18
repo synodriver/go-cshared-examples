5ebc4323f2dbfe3e83e3e63638a5064a51235e6c
Sourcery AI <>
2022-05-18 06:25:20 +0000

    'Refactored by Sourcery'

--- client.py
+++ client.py
@@ -13,8 +13,6 @@ lib.Cosine.argtypes = [c_double]
 lib.Cosine.restype = c_double
 print("awesome.Cosine(1) = %f" % lib.Cosine(1))
 
-# define class GoSlice to map to:
-# C type struct { void *data; GoInt len; GoInt cap; }
 class GoSlice(Structure):
     _fields_ = [("data", POINTER(c_void_p)), ("len", c_longlong), ("cap", c_longlong)]
 
@@ -24,10 +22,11 @@ nums = GoSlice((c_void_p * 5)(74, 4, 122, 9, 12), 5, 5)
 lib.Sort.argtypes = [GoSlice]
 lib.Sort.restype = None
 lib.Sort(nums)
-print("awesome.Sort(74,4,122,9,12) = %s" % [nums.data[i] for i in range(nums.len)])
+print(
+    f"awesome.Sort(74,4,122,9,12) = {[nums.data[i] for i in range(nums.len)]}"
+)
 
-# define class GoString to map:
-# C type struct { const char *p; GoInt n; }
+
 class GoString(Structure):
     _fields_ = [("p", c_char_p), ("n", c_longlong)]
 
